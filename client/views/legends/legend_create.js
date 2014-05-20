4638c91cde460b235d10ca312845610dd00b1be2
Gavin Thompson <gavinathompson@gmail.com>
2014-05-19 21:00:15 -0400

    Build legend post method to submit new legends; errors now found in collections/legend and new method in legendcreate.js

--- client/views/legends/legend_create.js
+++ client/views/legends/legend_create.js
@@ -1,15 +1,23 @@
 Template.legendCreate.events({ 
+
 	'submit form': function(e) {
 	    e.preventDefault();
+
 		var legend = {
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val(), 
 			message: $(e.target).find('[name=message]').val()
 		}
 	
-	legend._id = Legends.insert(legend);
-	
-	Router.go('legendPage', legend);
-	
+		// Meteor.call('post', post, function(error, id) { if (error)
+		Meteor.call('post', legend, function(error, id) {
+
+			if(error){
+				return alert(error.reason);
+			}
+
+		    
+		    Router.go('legendPage', {_id: id});
+		});
 	}
 });
--- collections/legends.js
+++ collections/legends.js
@@ -1,7 +1,37 @@
 Legends = new Meteor.Collection('legends');
 
-Legends.allow({
-	insert: function(userId, doc) {
-	    // only allow create legends if you are logged in
-	return !! userId; }
+Meteor.methods({
+	post: function(legendAttributes) {
+		var user = Meteor.user(),
+			legendWithSameLink = Legends.findOne({url: legendAttributes.url});
+
+    	// ensure the user is logged in
+		if (!user){
+			throw new Meteor.Error(401, "You need to login to post new legends");
+		}
+		// ensure the post has a title
+		if (!legendAttributes.title){
+			throw new Meteor.Error(422, 'Please fill in a title');
+		}
+
+	    // check that there are no previous posts with the same link
+		if (legendAttributes.url && legendWithSameLink) { 
+			throw new Meteor.Error(
+				302, 
+				'This link has already been used', 
+				legendWithSameLink._id
+			);
+    	}
+
+	    // pick out the whitelisted keys
+		var legend = _.extend(_.pick(legendAttributes, 'url', 'title', 'message'), { 
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+		
+		var legendId = Legends.insert(legend);
+		return legendId; 
+
+	}
 });

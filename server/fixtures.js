f5ad8cd89937f28ad8df9f8e20a6560c87e9ba70
Gavin Thompson <gavinathompson@gmail.com>
2014-05-21 19:47:02 -0400

    Added comments collection, pub/sub and fixtures.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,13 +1,57 @@
-if (Legends.find().count() === 0) { Legends.insert({
-	title: 'Introducing Telescope',
-	author: 'Sacha Greif',
-	url: 'http://sachagreif.com/introducing-telescope/'
-});
-Legends.insert({
-	title: 'Meteor',
-	author: 'Tom Coleman', url: 'http://meteor.com'
-});
-Legends.insert({
-	title: 'The Meteor Book', author: 'Tom Coleman',
-	url: 'http://themeteorbook.com'
-}); }
+if (Legends.find().count() === 0) { 
+
+	var now = new Date().getTime();
+
+	// create two users
+	var tomId = Meteor.users.insert({ 
+		profile: { name: 'Tom Coleman' }
+	});
+	var tom = Meteor.users.findOne(tomId); 
+
+	var sachaId = Meteor.users.insert({
+	    profile: { name: 'Sacha Greif' }
+	  });
+	var sacha = Meteor.users.findOne(sachaId);
+
+
+
+	var telescopeId = Legends.insert({
+		title: 'Introducing Telescope',
+		userId: sacha._id,
+		author: sacha.profile.name,
+		url: 'http://sachagreif.com/introducing-telescope/', 
+		submitted: now - 7 * 3600 * 1000
+	});
+
+	Comments.insert({
+		legendId: telescopeId,
+		userId: tom._id,
+		author: tom.profile.name,
+		submitted: now - 5 * 3600 * 1000,
+		body: 'Interesting project Sacha, can I get involved?'
+	});
+	
+	Comments.insert({
+		legendId: telescopeId,
+		userId: sacha._id,
+		author: sacha.profile.name, submitted: now - 3 * 3600 * 1000, 
+		body: 'You sure can Tom!'
+	});
+
+	Legends.insert({
+		title: 'Meteor',
+		userId: tom._id,
+		author: tom.profile.name,
+		url: 'http://meteor.com',
+		submitted: now - 10 * 3600 * 1000
+	});
+
+	Legends.insert({
+		title: 'The Meteor Book',
+		userId: tom._id,
+		author: tom.profile.name,
+		url: 'http://themeteorbook.com', 
+		submitted: now - 12 * 3600 * 1000
+	}); 
+}
+
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('legends', function() { 
 	return Legends.find();
 });
+
+Meteor.publish('comments', function() { 
+	return Comments.find();
+});

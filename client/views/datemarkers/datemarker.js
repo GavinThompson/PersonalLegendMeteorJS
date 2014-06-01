8addfa6bfdc1c07f2f968ecf5a3ca778db67e8c1
Gavin Thompson <gavinathompson@gmail.com>
2014-06-01 01:50:24 -0400

    Adding basic colour manipulation with skrollr

--- client/views/datemarkers/datemarker.js
+++ client/views/datemarkers/datemarker.js
@@ -1,18 +1,51 @@
-Template.datemarker.helpers({ 
+Template.datemarker.helpers({
+
 	submittedText: function() {
 		return new Date(this.submitted).toString(); 
 	},
-	// test
+
 	markerID: function(){
 		// console.log(this._id)
 		return this._id
 	},
 
 	index: 0,
+
 	indexInc: function(){
 		return Template.datemarker.index++;
-	}
+	},
 
+	contentTop: 0,
+
+	dataTop: function(){
+		// calculate the scrollTop position of the Template....
+		// set Template.datemarker.contentTop to said calculation
+		// return numeric value
+	},
+
+	dataBottom: function(){
+		// calculate template total height ("#datemarker" +index) (....maybe index +1 needed?)
+		// Sum Template.datemarker.contentTop to line above calculation.
+
+		// return sum
+
+	},
+
+	bkgColour: function(){
+		hex = this.backgroundColour.substring(0, 7)
+		// return hex
+		console.log("hex")
+		console.log(hex)
+		
+		colour1 = tinycolor(hex)
+
+		colourRGB = colour1.toRgbString()
+		console.log(colourRGB)	
+
+		return colourRGB
+	},
+
+	
 
 });
 
@@ -23,5 +56,4 @@ Template.datemarker.rendered = function() {
     // is finished rendering.
 
     console.log( "test.....")
-
 };

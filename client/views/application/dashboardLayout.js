3ff46e6d2381666cc6b45a1a8ae657cfe3bf99d1
Gavin Thompson <gavinathompson@gmail.com>
2014-06-22 00:50:35 -0400

    Adding fading arrows to dashboard sidemenu

--- client/views/application/dashboardLayout.js
+++ client/views/application/dashboardLayout.js
@@ -5,17 +5,25 @@ Template.dashboardLayout.events({
 			width: "200px",
 			opacity: "0.4"
 		}, 500, 'easeOutBounce', function(){
-			// 
+			$("#dashboardSideArrow").fadeOut( 250, function(){
+				// 
+			})
+			$("#sideInner").fadeIn( 500 )
+
 		})
  		// TODO: show inner sidemenu div; hide arrow (to be implemented) 
 	},
 
 	'mouseleave #dashboardSide': function(e) {
 		$("#dashboardSide").animate({
-			width: "30px",
+			width: "38px",
 			opacity: "1.0"
 		}, 500, 'easeOutBounce', function(){
-			// 
+
+			$("#sideInner").fadeOut( 250, function(){
+				// 
+			})
+			$("#dashboardSideArrow").fadeIn( 500 )
 		})
 		// TODO: hide inner sidemenu div; show arrow (to be implemented)
 	}

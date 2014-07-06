849fba4422819a628bbb42c42baa222636a11afa
Gavin Thompson <gavinathompson@gmail.com>
2014-07-06 15:24:23 -0400

    Changed CSS for legend summary

--- client/views/legends/legend_page.js
+++ client/views/legends/legend_page.js
@@ -1,6 +1,14 @@
 Template.legendPage.helpers({ 
 	datemarkers: function() {
 		return Datemarkers.find({legendId: this._id}, {sort: {dateSpan: 1}});
+	},
+
+	legendFirstDate: function(){
+		// 
+	},
+
+	legendLastDate: function(){
+		// 
 	}
 });
 

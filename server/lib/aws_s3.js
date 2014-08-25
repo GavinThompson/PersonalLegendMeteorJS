ec42c7cb3967df3029db6503f4df105c8967ef8a
Gavin Thompson <gavinathompson@gmail.com>
2014-08-25 12:31:26 -0400

    Fixing package errors; temp commenting out aws_s3.js; specifying master-branch for Houston. Deleting accounts-entry and deleting/re-adding houston/iron-router to fix atmosphere error of pushing 0.9 iron-router version too early where it's not matching with 0.8 meteorjs version/meteorjs package versions

--- server/lib/aws_s3.js
+++ server/lib/aws_s3.js
@@ -1,9 +1,9 @@
 // If resizing is required - checkout s3 package branch: http://atmospherejs.com/package/S3-Images-Blaze
 
 S3.config = {
-    key: process.env.AMAZONKEY,
-    secret: process.env.AMAZONSECRET,
-    bucket: process.env.S3_BUCKET,
-    // directory: '/subfolder/' //This is optional, defaults to root
-    directory: '/'
+    // key: process.env.AMAZONKEY,
+    // secret: process.env.AMAZONSECRET,
+    // bucket: process.env.S3_BUCKET,
+    // // directory: '/subfolder/' //This is optional, defaults to root
+    // directory: '/'
 };

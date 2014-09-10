a7d48d1a82bd04e45a0421cbf9a04a4e29783d62
Gavin Thompson <gavinathompson@gmail.com>
2014-09-10 14:00:02 -0400

    Adding password and better referencing on fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -120,142 +120,162 @@ if (Legends.find().count() === 0) {
 		return newNumber
 	}
 
+	titlesArray = ["The autumn trip that changed my life", "Golf is a good walk spoiled.", "Don't quack like a duck, soar like an eagle.", "Do one thing every day that scares you.", "Hold fast to dreams", "What's meant to be will always find a way", "The flower that blooms in adversity", "Sometimes you wake up", "Sometimes the fall kills you",  "And sometimes, when you fall, you fly", "Life is a broken-winged bird", "You never have to change anything", "You got up in the middle of the night to write", "If you're reading this...", "Congratulations, you're alive", "If that's not something to smile about", "The unexamined life is not worth living", "We have to go back", "It's not the load that breaks you down", "Pain is temporary", "Quitting lasts forever", "I was never really insane. Except.." , "The things you do for yourself...", "It is good to love many things", "What you do makes a difference", "Love is the absence of judgment", "Ancient Egyptians had fifty words for sand", "Eskimos have a hundred words for snow", "There are no words for that", "There are years that ask", "That means that this goodbye", "I dream my painting", "I paint my dream."]
+
 	// create two users
 	var testUserID = Meteor.users.insert({ 
 		username: 'AdamRandor',
-		email: 'test@test.com',
-		password: 'testtesttest'
+		email: 'test@test.com'
 	});
+
+	Accounts.setPassword(testUserID, 'testtesttest') 
 	var testUser = Meteor.users.findOne(testUserID); 
+
+	var legendFixtureArray = []
 
 
 	var legend1 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[1],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend1  )
 
 	var legend2 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[2],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend2  )
 
 	var legend3 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[3],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend3  )
 
 	var legend4 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[4],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend4  )
 
 	var legend5 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[5],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend5  )
 
 	var legend6 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[6],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend6  )
 
 	var legend7 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[7],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend7  )
 
 	var legend8 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[8],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend8  )
 
 	var legend9 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[9],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend9  )
 
 	var legend10 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[10],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend10 )
 
 	var legend11 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[11],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend11 )
 
 	var legend12 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[12],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend12 )
+
+	var legend13 = Legends.insert({
+		title: titlesArray[13],
+		userId: testUser._id,
+		author: testUser.username,
+		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
+		submitted: now - numberIncrease() * 3600 * 1000,
+		chaptersCount: 3
+	});
+	legendFixtureArray.push( legend13 )
 
 	var legend14 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
+		title: titlesArray[14],
 		userId: testUser._id,
 		author: testUser.username,
 		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
 		submitted: now - numberIncrease() * 3600 * 1000,
 		chaptersCount: 3
 	});
+	legendFixtureArray.push( legend14 )
 
-	var legend15 = Legends.insert({
-		title: 'Skeletor Birthday Chronology',
-		userId: testUser._id,
-		author: testUser.username,
-		synopsis: "'This is a story about them,' says the man on the radio. And you are concerned, because this is not a story you were ever supposed to hear. Welcome to Night Vale. This is a story about them.",
-		submitted: now - numberIncrease() * 3600 * 1000,
-		chaptersCount: 3
-	});
-
-	
+	console.log("legend fixture array")
+	console.log( legendFixtureArray )
 
 
 
@@ -263,17 +283,11 @@ if (Legends.find().count() === 0) {
 
 	var addChapterSeeds = function(){
 
-		totalLegends = Legends.find().count()
+		legendsLength = legendFixtureArray.length
 
-
-		findLegendID = function(number){
-			legendID = "legend" + number
-			return legendID
-		}
-
-		for(i = 1; i < totalLegends; i++){
+		for(i = 1; i < legendsLength; i++){
 			Chapters.insert({
-				legendId: findLegendID(i),
+				legendId: legendFixtureArray[i],
 				userId: testUser._id,
 				author: testUser.username,
 				subtitle: 'It was a good year on the Bayou.',
@@ -285,7 +299,7 @@ if (Legends.find().count() === 0) {
 			});
 
 			Chapters.insert({
-				legendId: findLegendID(i),
+				legendId: legendFixtureArray[i],
 				userId: testUser._id,
 				author: testUser.username,
 				subtitle: 'Wondered whatever happened on that long, long day.',
@@ -297,7 +311,7 @@ if (Legends.find().count() === 0) {
 			});
 
 			Chapters.insert({
-				legendId: findLegendID(i),
+				legendId: legendFixtureArray[i],
 				userId: testUser._id,
 				author: testUser.username,
 				subtitle: 'It was a good day to see red.',

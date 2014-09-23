fcaa579a3d2b70ed816149ec28d4bf9b3d76f25f
Gavin Thompson <gavinathompson@gmail.com>
2014-09-23 14:34:12 -0400

    Fixing convertToBoolean.js helper - adding .toLowercase() catch

--- client/helpers/convertToBoolean.js
+++ client/helpers/convertToBoolean.js
@@ -1,9 +1,6 @@
 convertToBoolean = function( string ){
-	console.log("String test")
-	console.log( string )
+	string = string.toLowerCase();
 	var myBoolean = (string === "true");
-	console.log("myBoolean Test")
-	console.log(myBoolean)
 	// if value is "true", will produce true ... if it's anything else (including "false") - will be false 
 	return myBoolean;
-}
+};

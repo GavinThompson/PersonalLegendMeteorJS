1094eababde3315b8f386fb03382427389f9497c
Gavin Thompson <gavinathompson@gmail.com>
2014-09-30 11:40:52 -0400

    Fixing legend view layout issues and bootstrap conflicts

--- client/views/legends/includes/legendViewSideNav.js
+++ client/views/legends/includes/legendViewSideNav.js
@@ -1,3 +1,29 @@
 Template.legendViewSideNav.helpers({
+	chapters: function() {
+		return Chapters.find({legendId: this._id, published: true}, {sort: {dateSpan: 1}});
+	},
 
+	chaptersWithTOCIndex: function() {
+
+		var chapters = Chapters.find({legendId: this._id, published: true}, {sort: {dateSpan: 1}});
+	    
+	    for(var i = 0; i < chapters.length; i++) {
+	    	// Table of contents start on '1' - so add '2' to 'i'
+	        chapters[i].tableOfContentIndex = i+2;
+	    }
+
+	    return chapters;
+	},
+
+	condensedChapterTitle: function() { 
+		fullTitle = this.subtitle
+		titleLength = fullTitle.length
+		if(titleLength >= 14){
+			newTitle = fullTitle.slice(0, 16)
+			newTitle = newTitle + "..."
+			return newTitle
+		}else{
+			return fullTitle
+		}
+	}
 });
